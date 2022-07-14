2626e6e911fa8d76f53e3f9bcee3deaa15c00511
thfilipe <thomasfilipe@yahoo.com>
2022-07-14 20:06:09 +0100

    Remove redundant functions and add test functions

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const buttons = document.getElementById('buttons');
+const numberButtons = document.getElementsByClassName('number');
+const operationButtons = document.getElementsByClassName('operation');
+const equalsButton = document.getElementById('equals');
+const allClearButton = document.getElementById('all-clear');
+const backButton = document.getElementById('back');
+const totalButton = document.getElementById('total');
+const calculationButton = document.getElementById('calculation');
+
+numberButtons.forEach(number => {
+    console.log(number)
+})
+
+function press() {
+    calculationButton.textContent += "test";
+}
+function allClear() {
+    calculationButton.textContent = "";
+    totalButton.textContent = "";
+}
 
 function operate(operator, num1, num2) {
 
-}
+};
 
 function add(num1, num2) {
     return num1 + num2
@@ -20,28 +38,11 @@ function power(num, power) {
     return Math.pow(num, power)
 };
 
-function sum(num) {
-    if (num.length === 0) {
-        return 0;
-    }
-    return num.reduce((acc, curr) => acc + curr)
-};
-
 function divide(num1, num2) {
     return num1 / num2
 }
 
-function factorial(fact) {
 
-    if (fact == 0 || fact == 1) {
-        return 1;
-    }
-    let sum = 1
-    for (i = 1; i < fact; i++) {
-        sum = sum * (i + 1);
-    }
-    return sum
-};
 
 
 
