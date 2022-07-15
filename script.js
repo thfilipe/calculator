b5571273f383b0a73712ad6d21ddf7e0e231914c
thfilipe <thomasfilipe@yahoo.com>
2022-07-15 16:07:35 +0100

    Remove redundant functions and add function to handle number buttons

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
-const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const allClearButton = document.querySelector('[data-all-clear]');
-const deleteButton = document.querySelector('[data-delete]');
-const totalButton = document.querySelector('[data-total]');
-const calculationButton = document.querySelector('[data-calculation]');
+const totalText = document.querySelector('#total');
+const calculateText = document.querySelector('#calculate');
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('#equals');
+const allClearButton = document.querySelector('#all-clear');
+const backButton = document.querySelector('#back');
 
-numberButtons.forEach(number => {
-    console.log(number)
-})
 
-function press() {
-    calculationButton.textContent += "test";
+let totalOperand = '';
+let calculateOperand = '';
+let operation = null;
+
+function handleButtons() {
+    numberButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (button.textContent === "." && calculateOperand.includes('.')) return;
+            calculateOperand += button.textContent;
+            updateDisplay();
+        })
+
+    })
 }
+
+function updateDisplay() {
+    calculateText.textContent = calculateOperand;
+}
+
+handleButtons()
+
 function allClear() {
-    calculationButton.textContent = "";
-    totalButton.textContent = "";
+    calculateText.textContent = '';
+    totalText.textContent = '';
+    totalOperand = '';
+    calculateOperand = '';
 }
-
-function operate(operator, num1, num2) {
-
-};
 
 function add(num1, num2) {
-    return num1 + num2
+    return num1 + num2;
 };
 
 function subtract(num1, num2) {
-    return num1 - num2
+    return num1 - num2;
 };
 
 function multiply(num1, num2) {
-    return num1 * num2
+    return num1 * num2;
 };
 
 function power(num, power) {
-    return Math.pow(num, power)
+    return Math.pow(num, power);
 };
 
 function divide(num1, num2) {
-    return num1 / num2
+    return num1 / num2;
 }
 
 
